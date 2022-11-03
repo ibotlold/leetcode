3d2f94089312d3c647a6b0e3a173348f317c00b5
Kirill <ibotlold@gmail.com>
2022-11-03 16:45:16 +0600

    ref: moved 2406 solution to `Main.java`

--- solutions/2406/src/Main.java
+++ solutions/2406/src/Main.java
@@ -1,3 +1,7 @@
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 public class Main {
     public static void main(String[] args) {
         Solution minimumNumberGroups = new Solution();
@@ -7,25 +11,62 @@ public class Main {
     }
 }
 
-class Solution {
+public class Solution {
     public int minGroups(int[][] intervals) {
-        int result = 1;
-        int mask = 0;
-        for (int i = 0; i < intervals.length - 1; i++) {
-            int intersections = 0;
-            for (int j = i + 1; j < intervals.length; j++) {
-                if ((mask & (1 << i)) != 0) continue;
-                if (intersect(intervals[i], intervals[j])) intersections++;
-                mask = mask | (1 << i);
-            }
-            result = Math.max(result, intersections);
+        Arrays.sort(intervals, Comparator.comparingInt(a -> a[0]));
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+        for (int[] interval :
+                intervals) {
+            if (!queue.isEmpty() && queue.peek() < interval[0]) queue.remove();
+            queue.offer(interval[1]);
         }
-        return result;
-    }
-
-    boolean intersect(int[] a, int[] b) {
-        if (a[0] > b[1]) return false;
-        if (a[1] < b[0]) return false;
-        return true;
+        return queue.size();
     }
 }
+
+
+// class Solution {
+//     public int minGroups(int[][] intervals) {
+//         int result = 1;
+//         int mask = 0;
+//         for (int i = 0; i < intervals.length - 1; i++) {
+//             int intersections = 0;
+//             for (int j = i + 1; j < intervals.length; j++) {
+//                 if ((mask & (1 << i)) != 0) continue;
+//                 if (intersect(intervals[i], intervals[j])) intersections++;
+//                 mask = mask | (1 << i);
+//             }
+//             result = Math.max(result, intersections);
+//         }
+//         return result;
+//     }
+
+//     boolean intersect(int[] a, int[] b) {
+//         if (a[0] > b[1]) return false;
+//         if (a[1] < b[0]) return false;
+//         return true;
+//     }
+// }
+
+// class Solution {
+//     public int minGroups(int[][] intervals) {
+//         int result = 1;
+//         int mask = 0;
+//         for (int i = 0; i < intervals.length - 1; i++) {
+//             int intersections = 0;
+//             for (int j = i + 1; j < intervals.length; j++) {
+//                 if ((mask & (1 << i)) != 0) continue;
+//                 if (intersect(intervals[i], intervals[j])) intersections++;
+//                 mask = mask | (1 << i);
+//             }
+//             result = Math.max(result, intersections);
+//         }
+//         return result;
+//     }
+
+//     boolean intersect(int[] a, int[] b) {
+//         if (a[0] > b[1]) return false;
+//         if (a[1] < b[0]) return false;
+//         return true;
+//     }
+// }
