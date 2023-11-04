e806a689e882d4c7e018c6ee94ccbc75be533de2
Kirill <ibotlold@gmail.com>
2023-11-04 13:52:25 +0600

    refactor(1503): iterator

--- solutions/lc1503/src/Solution.java
+++ solutions/lc1503/src/Solution.java
@@ -1,11 +1,11 @@
 class Solution {
     public int getLastMoment(int n, int[] left, int[] right) {
         int max = 0;
-        for (int i = 0; i < right.length; i++) {
-            max = Math.max(max, n - right[i]);
+        for (int i : right) {
+            max = Math.max(max, n - i);
         }
-        for (int i = 0; i < left.length; i++) {
-            max = Math.max(max, left[i]);
+        for (int i : left) {
+            max = Math.max(max, i);
         }
         return max;
     }
