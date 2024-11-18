2779e02cf4133929d6a24c4afc7f7c268c55b876
Kirill Prokopev <ibotlold@gmail.com>
2024-11-18 22:40:49 +0500

    refactor(13): map to switch and change algorithm
    use hint. traversing backwards

--- solutions/lc13/main.go
+++ solutions/lc13/main.go
@@ -1,39 +1,42 @@
 package main
 
+import "slices"
+
 // 13. Roman to Integer
 // https://leetcode.com/problems/roman-to-integer
 
 func romanToInt(s string) int {
-	romanMap := map[string]int{
-		"I":  1,
-		"IV": 4,
-		"V":  5,
-		"IX": 9,
-		"X":  10,
-		"XL": 40,
-		"L":  50,
-		"XC": 90,
-		"C":  100,
-		"CD": 400,
-		"D":  500,
-		"CM": 900,
-		"M":  1000,
-	}
-
-	var result, first, second int
-	for first < len(s) {
-		if first+1 < len(s) {
-			second = first + 1
-			val, ok := romanMap[s[first:second+1]]
-			if ok {
-				result += val
-				first = second + 1
-				continue
-			}
+	var backward = slices.Backward([]byte(s))
+	var prev, sum int
+	for _, v := range backward {
+		val := letter(v)
+		if prev > val {
+			sum -= val
+		} else {
+			sum += val
 		}
-		result += romanMap[s[first:first+1]]
-		first++
+		prev = val
 	}
 
-	return result
+	return sum
+}
+
+func letter(b byte) int {
+	switch b {
+	case 'I':
+		return 1
+	case 'V':
+		return 5
+	case 'X':
+		return 10
+	case 'L':
+		return 50
+	case 'C':
+		return 100
+	case 'D':
+		return 500
+	case 'M':
+		return 1000
+	}
+	return 0
 }
