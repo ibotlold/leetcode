fbf7dfcdeaa841b04cf2e61f478089a4a4263c53
Kirill Prokopev <ibotlold@gmail.com>
2024-11-21 21:54:06 +0500

    refactor(238): one loop to traverse

--- solutions/lc238/main.go
+++ solutions/lc238/main.go
@@ -4,23 +4,23 @@ package main
 // https://leetcode.com/problems/product-of-array-except-self/
 
 func productExceptSelf(nums []int) []int {
-	prefix := make([]int, len(nums))
-	ball := 1
-	prefix[0] = 1
-	for i := 1; i < len(nums); i++ {
-		prefix[i] = nums[i-1] * ball
-		ball = prefix[i]
+	n := len(nums)
+	answer := make([]int, n)
+	postfix := make([]int, n)
+	answer[0] = 1
+	postfix[n-1] = 1
+
+	prefixProduct := 1
+	postfixProduct := 1
+	for i := 1; i < n; i++ {
+		prefixProduct *= nums[i-1]
+		postfixProduct *= nums[n-i]
+		answer[i] = prefixProduct
+		postfix[n-i-1] = postfixProduct
 	}
-	postfix := make([]int, len(nums))
-	ball = 1
-	postfix[len(nums)-1] = 1
-	for i := len(nums) - 2; i >= 0; i-- {
-		postfix[i] = nums[i+1] * ball
-		ball = postfix[i]
+	for i := 0; i < n; i++ {
+		answer[i] = answer[i] * postfix[i]
 	}
-	result := make([]int, len(nums))
-	for i := 0; i < len(result); i++ {
-		result[i] = prefix[i] * postfix[i]
-	}
-	return result
+
+	return answer
 }
