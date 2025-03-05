609b355bd27be7fcfe331191e87704e0e0e6e104
Kirill Prokopev <ibotlold@gmail.com>
2025-03-05 23:49:31 +0500

    refactor(110): mul return value

--- solutions/lc110/main.go
+++ solutions/lc110/main.go
@@ -22,9 +22,8 @@ func isBalanced(root *TreeNode) bool {
 	if root == nil {
 		return true
 	}
-	left := height(root.Left)
-	right := height(root.Right)
-	return abs(left-right) <= 1 && isBalanced(root.Left) && isBalanced(root.Right)
+	_, ok := process(root)
+	return ok
 }
 
 func abs(a int) int {
@@ -34,11 +33,17 @@ func abs(a int) int {
 	return a
 }
 
-func height(root *TreeNode) int {
+func process(root *TreeNode) (int, bool) {
 	if root == nil {
-		return 0
+		return 0, true
 	}
-	left := height(root.Left)
-	right := height(root.Right)
-	return 1 + max(left, right)
+	left, ok := process(root.Left)
+	if !ok {
+		return 0, false
+	}
+	right, ok := process(root.Right)
+	if !ok || abs(left-right) > 1 {
+		return 0, false
+	}
+	return 1 + max(left, right), true
 }
