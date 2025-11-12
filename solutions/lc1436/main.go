4f9aa29144cac9b4f01d335dd9fc7b8eec0eb1f9
Kirill Prokopev <ibotlold@gmail.com>
2025-11-12 22:09:09 +0500

    refactor(1436): outgoing check

--- solutions/lc1436/main.go
+++ solutions/lc1436/main.go
@@ -4,16 +4,15 @@ package main
 // https://leetcode.com/problems/destination-city/description/
 
 func destCity(paths [][]string) string {
-	m := make(map[string]int)
+	m := make(map[string]bool)
 
 	for _, p := range paths {
-		m[p[0]] = m[p[0]] - 1
-		m[p[1]] = m[p[1]] + 1
+		m[p[0]] = true
 	}
 
-	for k, i := range m {
-		if i == 1 {
-			return k
+	for _, p := range paths {
+		if v, ok := m[p[1]]; !ok {
+			return p[1]
 		}
 	}
 
